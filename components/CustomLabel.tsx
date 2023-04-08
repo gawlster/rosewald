30dc6de03eaa79201421db1c5521f8000591ab94
Connor Gawley <connorgawley@uvic.ca>
2023-04-08 15:12:29 -0700

    remove old stuff no longer in use

--- components/CustomLabel.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const CustomLabel: React.FC<{ children: any }> = ({ children }) => {
-    return <label className='flex flex-col'>{children}</label>
-}
-
-export default CustomLabel
